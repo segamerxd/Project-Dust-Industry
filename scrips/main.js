f0ad26962baeb456ed3d25053265b480c28de81a
Arkanic <93954648+Arkanicjs@users.noreply.github.com>
2022-02-27 23:21:20 -0400

    Create main.js

---
+++ scrips/main.js
@@ -0,0 +1,26 @@
+function req(){
+  let args = arguments
+  for(let i in args){
+    let dir = ""
+    if(args[i][0] != ""){
+      dir = args[i][0] + "/"
+    }
+    for(let j in args[i]){
+      if(args[i][0] != args[i][j]){
+        require("heavymachinery/" + dir + args[i][j])
+        print("heavymachinery/" + dir + args[i][j])
+      }
+    }
+  }
+}
+
+req(
+  //Libs
+  ["libs", "drawlib", "effectlib", "ai", "function", "bulletTypes", "blockTypes", "planetGen", "statLists"],
+  //No Directory
+  ["", "items", "units", "statusEffects"],
+  //Campaign
+  ["campaign", "planets"],
+  //Blocks
+  ["blocks", "content-blocks-production", "miniCore", "reconstructors", "projectors", "turrets", "walls"]
+)
