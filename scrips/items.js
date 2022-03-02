90c7a1f4ecbaa51c865af027b1892abcd1f56002
Arkanic <93954648+Arkanicjs@users.noreply.github.com>
2022-03-02 19:59:01 -0400

    Delete items.js

--- scrips/items.js
+++
@@ -1,9 +0,0 @@
-const astral = extend(Item, "astral", {});
-const acid_ash = extend(Item, "acid ash", {});
-
-const citem = stringName => Vars.content.getByName(ContentType.item, "projectindustry-" + stringName);
-
-module.exports = {
-	astral: citem("astral"),
-	acid_ash: citem("acid ash")
-};
